f52c779e6dd01a9b3563a04839f23ef2a8d89880
skmayya1 <skandamayya4@gmail.com>
2025-02-18 09:08:06 +0530

    feat:Auth setup

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { authClient } from '@/lib/auth-client';
+import React, { useEffect } from 'react'
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+
+const URL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL
+
+const Page = () => {
+  const router = useRouter();
+    const { data, isPending } = authClient.useSession();
+    const session = data?.session;
+    
+    useEffect(() => {
+    if (!isPending && session) {
+      router.push("/");
+    }
+  }, [session, isPending, router]);
+
+  if (isPending || (session && session)) return null;
+
+  return (
+      <div className='h-screen w-full'>
+          <button onClick={async () => {
+              toast.promise(
+                  async () => { 
+                  await authClient.signIn.social({
+                      provider: "github",
+                })}
+                , {
+                    loading: "Authenticating...",
+                    success: "Signed in successfully",
+                    error: "Something went wrong"
+                 })
+          }}>
+              Sign In With Github 
+          </button>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+    </div>
+  ) 
+}
+
+export default Page

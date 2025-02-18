24c1c4fc5563ceea268b7ac88198c350e586839f
skmayya1 <skandamayya4@gmail.com>
2025-02-18 16:37:56 +0530

    feat:Repo section

--- src/Dashboard/Components/Dashboard.tsx
+++ src/Dashboard/Components/Dashboard.tsx
@@ -1,17 +1,24 @@
 import { authClient } from '@/lib/auth-client';
+import { Repos } from '@/Repos/Components/Repos';
+import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+
+        const router = useRouter();
         const { data, isPending } = authClient.useSession();
         const session = data?.session;
-    useEffect(() => {
-        console.log(session);
-        console.log(data);
+  
+        useEffect(() => {
+          if (!session && !isPending) {
+            router.push('/auth/signin');
+          }
+        }, [session, isPending, router]);
         
-        
-     }, [session , data]);
   return (
-    <div>Dashboard</div>
+    <div className='h-screen w-full'>
+      <Repos/>
+    </div>
   )
 }
 
--- src/Repos/Components/Repos.tsx
+++ src/Repos/Components/Repos.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Toast } from '@/lib/Toast';
+import { RepoResponse } from '@/Types/api';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const Repos = () => {
+export const Repos = () => {
+  const [ReposData, setReposData] = useState<RepoResponse[] | null>(null)
+  
+        useEffect(() => {
+            async function fetchData() { 
+              const res = await axios.get('/api/repos');
+              const repos = await res.data;
+              if (repos.message) {
+                Toast(repos.message, 'error');
+              }
+              setReposData(repos);
+            }
+            fetchData();
+        }
+        , []);
+        
   return (
-    <div>Repos</div>
+    <div>
+      
+    </div>
   )
 }
-export default Repos
+
